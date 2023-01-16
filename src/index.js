fa653166219092c920fdb24472c53f91e66e00e7
katrina Young <hello@katrinayoungconsulting.com>
2023-01-16 14:23:34 +0000

    homework for classwork submitted and not committed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export const projectName = () => {
   return 'Kata starter project';
 };
+
+export const fizzbuzz = () => {
+  return '1';
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,13 @@
-import {projectName} from "./index";
+import {projectName,fizzbuzz} from "./index";
 
 describe('Starter project should', () => {
   it('output its name', () => {
     expect(projectName()).toEqual('Kata starter project');
+  });
+});
+
+describe('fizzbuzzfunction', () => {
+  it('should output 1 when given 1', () => {
+    expect(fizzbuzz(1)).toEqual('1');
   });
 });
